1cd351a76f6fba7cac36d56d6604326569b3257b
Ariel <74215813+se7enwin@users.noreply.github.com>
2025-06-04 20:59:25 +0200

    Update App.Jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,6 @@ export default function App() {
 
     return (
 
-        <h3>Function Component from Local Jsx </h3>
+        <h3 style={{ color: 'green' }}>Function Component from Local Jsx </h3>
     )
 }
