ef0e17ebc0f1cbe7bd3e00ab10d9c157abc0d19d
Ariel <74215813+se7enwin@users.noreply.github.com>
2025-06-06 23:18:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import App from './App.jsx';
 import root from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+
+// Where to render
 const tag = document.getElementById('root');
 const run = root.createRoot(tag);
+// What to render
 run.render(<BrowserRouter><App /></BrowserRouter>);
